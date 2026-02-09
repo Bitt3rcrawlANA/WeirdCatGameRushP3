9ff873366b987bb662a45cf991a0c81562e48c00
ethancastaneda1 <726750@student.dusd.net>
2026-02-09 11:58:53 -0800

    began enemy movement
    ew

---
+++ WeirdCatGameP3Rush/Assets/Scripts/Glires.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Glires : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb2d;
+    private Transform currentPoint;
+    public float speed;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb2d.linearVelocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb2d.linearVelocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Field of View Entered!");
+    }
+}
